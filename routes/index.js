1a8b7d9ecb4390d4e5fe4f9806d1e8ea2739455c
Lubo-10 <bernlubo01@gmail.com>
2022-07-28 20:27:10 -0400

    user-model

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// const router = require('express').Router();
+// const apiRoutes = require('./api');
+
+// router.use('/api', apiRoutes);
+
+// router.use((req, res) => {
+//     res.status(404).send('<h1>routes not working in index.js</h1>')
+// })
+
+// module.exports = router;
